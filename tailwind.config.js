3980230e2a9e55705049cca4f5d587fc1ca67e55
Akhmad Salafudin <axmad386@gmail.com>
2022-09-04 15:15:50 +0700

    chore: using tailwindcss as postcss plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,15 +6,22 @@ module.exports = {
       backgroundColor: {
         primary: "var(--bg-primary)",
         secondary: "var(--bg-secondary)",
-        accent: "var(--bg-accent)",
+        accent: "rgb(var(--bg-accent)/var(--tw-bg-opacity))",
       },
       colors: {
         primary: "var(--c-primary)",
         secondary: "var(--c-secondary)",
         accent: "var(--c-accent)",
+        gray: {
+          100: "var(--c-gray-100)",
+          800: "var(--c-gray-800)",
+        },
       },
       fontFamily: {
         sans: "var(--font-sans)",
+      },
+      fontSize: {
+        title: "32px",
       },
     },
   },
